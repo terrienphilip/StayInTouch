bd42339a244e9110681ef3467c761423fcc0617c
Philip Terrien <terrienphilip@gmail.com>
2018-02-21 22:27:48 -0600

    Setup the user interface to load on the queue of contacts

--- app/src/main/java/com/example/philipterrien/stayintouch/MainActivity.java
+++ app/src/main/java/com/example/philipterrien/stayintouch/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.philipterrien.stayintouch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = findViewById(R.id.addContactBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
     }
 }
